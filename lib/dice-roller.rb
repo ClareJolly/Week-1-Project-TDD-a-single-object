0b16db332a0d2cf280d7e2fe11a7778e217711e7
Clare Jolly <clarejollyuk@gmail.com>
2018-10-02 13:03:53 +0100

    readme update

--- lib/dice-roller.rb
+++ lib/dice-roller.rb
@@ -1,10 +1,14 @@
 def dice_roll(num_dice = 1)
-  dice = [1, 2, 3, 4, 5, 6]
   results = []
   num_dice.times {
-  results << dice.sample
+    results << randomize
   }
   return results.reduce(:+)
+end
+
+def randomize
+  dice = [1, 2, 3, 4, 5, 6]
+  return dice.sample
 end
 
 # puts dice_roll(2)
--- spec/dice-roller-spec.rb
+++ spec/dice-roller-spec.rb
@@ -1,16 +1,16 @@
 require 'dice-roller'
 
-describe "dice_roll" do
+describe "randomize" do
   it "returns an integer - one dice" do
     #expect(dice_roll).to eq(1).or eq(2).or eq(3).or eq(4).or eq(5).or eq(6)
-    expect(dice_roll).to be_a_kind_of(Integer)
+    expect(randomize).to be_a_kind_of(Integer)
   end
 end
 
-describe "dice_roll" do
+describe "randomize" do
   it "returns a number from 1 to 6 - one dice" do
     #expect(dice_roll).to eq(1).or eq(2).or eq(3).or eq(4).or eq(5).or eq(6)
-    expect(dice_roll).to be_between(1, 6).inclusive
+    expect(randomize).to be_between(1, 6).inclusive
   end
 end
 
@@ -22,7 +22,7 @@ describe "dice_roll" do
 end
 
 describe "dice_roll" do
-  it "return result from 2 to 12 for 2 dice" do
+  it "return result from 4 to 24 for 4 dice" do
     #expect(dice_roll).to eq(1).or eq(2).or eq(3).or eq(4).or eq(5).or eq(6)
     expect(dice_roll(4)).to be_between(4, 24).inclusive
   end
